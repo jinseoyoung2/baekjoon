8ffa1ec3b2a224f64f9b6e437581f66aa9df5ab3
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2021-12-25 16:32:47 +0900

    Update 10699_오늘날짜.java

--- java/10699_오늘날짜.java
+++ java/10699_오늘날짜.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 public class Main{
     public static void main(String[] args){
-        LocalDate date = LocalDate.now();
+        LocalDate date = LocalDate.now(); //LocalDate.now()는 날짜 정보 가져오기 YYYY-MM-DD 형식
         System.out.print(date);
     }
 }
