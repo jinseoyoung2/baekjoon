b4bd16a32281fcf260412df7b11e2ef7fba86434
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2022-01-10 19:27:33 +0900

    Update 11719_그대로 출력하기 2.java

--- java/11719_그대로 출력하기 2.java
+++ java/11719_그대로 출력하기 2.java
@@ -1,8 +1,10 @@
+// https://www.acmicpc.net/problem/11719
+
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()){
+        while(sc.hasNextLine()){ //hasNextLine() => 다음에 읽을 값이 있는지 확인하여 불린타입으로 반환(값이 있을 경우 true를 반환한다.)
             System.out.println(sc.nextLine());
         }
     }
