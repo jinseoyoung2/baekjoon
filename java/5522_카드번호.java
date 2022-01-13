4285e6eba0afcf9cefd380ad38b728c6a8ca0f73
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2022-01-13 22:40:05 +0900

    Add files via upload

---
+++ java/5522_카드번호.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int res = 0;
+        for(int i=0; i<5; i++){
+            res += sc.nextInt();
+        }
+        System.out.print(res);
+    }
+}
