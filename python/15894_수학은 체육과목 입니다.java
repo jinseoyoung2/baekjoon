16690f2599c044ebbce6011d36949bab9cc05265
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2022-01-15 21:46:07 +0900

    Delete 15894_수학은 체육과목 입니다.java

--- python/15894_수학은 체육과목 입니다.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-import java.math.BigInteger;
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        BigInteger bigInteger = sc.nextBigInteger();
-        System.out.print(bigInteger.multiply(BigInteger.valueOf(4)));
-    }
-}
