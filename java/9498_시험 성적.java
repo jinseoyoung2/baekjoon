b05f76073e8f519d83e90a4f7ed19a2e8b959b35
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2022-01-16 18:20:41 +0900

    Add files via upload

---
+++ java/9498_시험 성적.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        switch (num/10){
+            case 10:
+            case 9:
+                System.out.print("A");
+                break;
+            case 8:
+                System.out.print("B");
+                break;
+            case 7:
+                System.out.print("C");
+                break;
+            case 6:
+                System.out.print("D");
+                break;
+            default:
+                System.out.print("F");
+                break;
+        }
+    }
+}
