31178cfc758fc973e6bdf3f44038eedbf247cfc7
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2022-02-04 04:13:34 +0900

    Add files via upload

---
+++ java/2164_카드2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Queue<Integer> NList = new LinkedList<Integer>();
+        for(int i=1; i<=N; i++){ NList.add(i); }
+        while(NList.size()!=1){
+            NList.poll();
+            NList.add(NList.poll());
+        }
+        System.out.print(NList.poll());
+    }
+}
