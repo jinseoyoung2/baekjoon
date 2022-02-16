b7c8a07adc63390cbeff0b0e40c316f1a41159f3
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2022-02-16 21:50:10 +0900

    Add files via upload

---
+++ java/2884_알람 시계.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        m-=45;
+        if(m<0){
+            if(h==0){
+                System.out.print("23 "+(m+60));
+            }else{
+                System.out.print((h-1)+" "+(m+60));
+            }
+        }else{
+            System.out.print(h+" "+m);
+        }
+    }
+}
