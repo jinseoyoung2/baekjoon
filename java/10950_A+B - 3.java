312dc39a7e23b518510b5ae7c0c8969e51c06153
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2022-02-22 16:12:30 +0900

    Add files via upload

---
+++ java/10950_A+B - 3.java
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/10950
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
+        for(int i=0; i<testCase; i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A+B);
+        }
+    }
+}
